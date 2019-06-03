d6118c939c59cdb7b3871e3e4297d49b3d2cc282
kentowatanabe <kntbot@gmail.com>
2019-06-03 16:50:20 +0900

    feat: Download tweet image

--- TweetCrawler/TweetCrawler/pipelines.py
+++ TweetCrawler/TweetCrawler/pipelines.py
@@ -1,11 +1,24 @@
 # -*- coding: utf-8 -*-
-
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import scrapy
+from scrapy.pipelines.images import ImagesPipeline
+from scrapy.utils.misc import md5sum
 
 
-class TweetcrawlerPipeline(object):
-    def process_item(self, item, spider):
-        return item
+class SaveImage(ImagesPipeline):
+    def get_media_requests(self, item, info):
+        for image_url in item['image_urls']:
+            yield scrapy.Request(image_url)
+
+    def image_downloaded(self, response, request, info):
+        checksum = None
+        for path, image, buf in self.get_images(response, request, info):
+            if checksum is None:
+                buf.seek(0)
+                checksum = md5sum(buf)
+            width, height = image.size
+            filename = request._url.rsplit("/", 1)[1]
+            path = '%s' % (filename)
+            self.store.persist_file(
+                path, buf, info,
+                meta={'width': width, 'height': height})
+        return checksum
